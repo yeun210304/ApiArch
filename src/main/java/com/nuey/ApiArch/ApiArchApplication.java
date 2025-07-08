f7aaffb229265529fd39a0b98989f0ee08f9b146
Yeun <yeun210304@gmail.com>
2025-07-09 00:36:57 +0900

    staarrrt

---
+++ src/main/java/com/nuey/ApiArch/ApiArchApplication.java
@@ -0,0 +1,13 @@
+package com.nuey.ApiArch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuey/ApiArch/ApiArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuey.ApiArch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
